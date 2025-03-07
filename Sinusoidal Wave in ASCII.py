9397b722db681a7d87f23a7324b5d8854940ea8a
Fikrul Ihsan Arifin <fikrul412@gmail.com>
2025-03-07 14:57:22 +0700

    as

--- Sinusoidal Wave in ASCII.py
+++ Sinusoidal Wave in ASCII.py
@@ -1,152 +1,45 @@
-import pygame
-import numpy as np
-from random import randint, choice
+import numpy as np 
+import time
+import os
+import keyboard
+size = 40
+m = [[0 for _ in range(size)] for _ in range(size)]
+m_text = "\n".join(str(row) for row in m)
 
-""" PYGAME TEMPLATE """
-pygame.init()
+A = 5
+w = 4
+B = 1
 
-WIDTH, HEIGHT = 800, 600
-FPS = 40
+def function(dt, A, w):
+    return A * np.cos(w*dt) + B * np.sin(w*dt)
 
-WHITE = (255, 255, 255)
-BLACK = (0, 0, 0)
-YELLOW = (243, 247, 111)
+class Point():
+    def __init__(self, xCor, yCor, val, screen):
+        self.x = xCor
+        self.y = yCor
+        self.val = val
+        self.screen = screen 
+        self.spawn(self.screen)
 
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Polar Coordinate")
-
-clock = pygame.time.Clock()
-
-running = True
-radius = 0
-theta = 0
-
-
-""" POLAR COORDINATE"""
-scale = 0
-start_pos = 60
-initiate_pos = start_pos + randint(0, 180)
-
-def xPos(r, theta):
-    return r*np.cos(theta)
-
-def yPos(r, theta):
-    return r*np.sin(theta)
-
-def thetaPos(x, y):
-    return np.arctan(y/x)
-
-def rPos(x, y):
-    return np.sqrt((x - WIDTH//2)**2 + (y + HEIGHT//2)**2)
-
-def generate_map():
-    global start_pos
-    global scale
-    scale = choice([-15,-11,8,-5, 5, 8, 11, 15])
-    initiate_pos = start_pos + randint(0, 180)
-    xCoordinates = [xPos((i+1)*0.1, (i+1)*0.001*scale + initiate_pos) for i in range(3000)]
-    yCoordinates = [yPos((i+1)*0.1, (i+1)*0.001*scale + initiate_pos) for i in range(3000)]
-    return [[x, y] for x, y in zip(xCoordinates, yCoordinates)]
-
-baseCoordinates = generate_map()
-""" TARGET COORDINATE """
-targetCoordinate = [[randint(50, 750), randint(25, 575)]]
-
-""" PLAYER CLASS """
-class Player():
-    def __init__(self, angle, r):
-        self.angle = angle
-        self.r = r
-        self.dt = 0
-        self.speed = 2
-    def move(self):
-        keyEvent = pygame.key.get_pressed()
-        # for event in pygame.event.get():
-        #     if event.type == pygame.KEYDOWN:
-        #         if event.key == pygame.K_SPACE:
-        #             self.r += 1
-        #             self.dt = 0 
-        if keyEvent[pygame.K_UP]:
-            self.r += 1*self.speed
-            self.dt = 0 
-        if keyEvent[pygame.K_DOWN]:
-            self.r -= 1*self.speed
-            self.dt = 0 
-        # if keyEvent[pygame.K_LEFT]:
-        #     self.angle -= 180/np.pi/self.r*0.1
-        # if keyEvent[pygame.K_LEFT]:
-        #     self.xPos -= 1
-        # if keyEvent[pygame.K_UP]: 
-        #     self.yPos -= 1
-        # if keyEvent[pygame.K_DOWN]:
-        #     self.yPos += 1
-    def moveAuto(self):
-        self.angle += 180/np.pi/self.r*0.01*scale
-        self.dt += 0.05
-        self.r -= self.dt
-
-        # self.angle += 0.008
-    def collision(self, listCoordinate):
-        global targetCoordinate
-        global baseCoordinates
-        for x, y in listCoordinate[::10]: 
-            distance = np.sqrt((WIDTH//2 + xPos(self.r, self.angle) - (WIDTH//2 + x))**2 + 
-                            (HEIGHT//2 + yPos(self.r, self.angle) - (HEIGHT//2 + y))**2)
-            if distance < 6:  
-                self.r = 60
-                self.angle = 60
-                self.dt = 0
-                baseCoordinates = generate_map()
-                targetCoordinate = [[randint(50, 750), randint(25, 575)]]
-                return
-    def collision2(self, listCoordinate):
-        global baseCoordinates
-        global targetCoordinate
-        for x, y in listCoordinate: 
-            distance = np.sqrt((WIDTH//2 + xPos(self.r, self.angle) - (x))**2 + 
-                            (HEIGHT//2 + yPos(self.r, self.angle) - (y))**2)
-            if distance < 10:  
-                self.r = 60
-                self.angle = 60
-                self.dt = 0
-                baseCoordinates = generate_map()
-                targetCoordinate = [[randint(50, 750), randint(25, 575)]]
-                return
-
-    def spawn(self):
-        pygame.draw.circle(screen, WHITE, (WIDTH//2 + xPos(self.r, self.angle), HEIGHT//2 + yPos(self.r, self.angle)), 5)
-
-player = Player(start_pos, 60)
-finish = False
-""" GAME LOOP """
-while running:
-    clock.tick(FPS)
+    def spawn(self, screen):
+        screen[self.x][self.y] = self.val
     
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-        if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_SPACE:
-                    player.speed = 4
-        elif event.type == pygame.KEYUP:
-                if event.key == pygame.K_SPACE:
-                    player.speed = 1
+list_point = [Point(7, i, 1, m) for i in range(size)]
+dt = 0
+while True:
+    if keyboard.is_pressed("q"):  
+        print("\nExiting animation...")
+        exit()
+    dt+= 0.1
+    os.system("cls" if os.name == "nt" else "clear")
+    m = [[0 for _ in range(size)] for _ in range(size)]
+    for i in range(len(list_point)):
+        list_point[i].x = round(size//2 + function(dt + i*0.1, A, w))
 
-    """UPDATE PLACE"""
-    
-    if not finish:
-        screen.fill(BLACK)
-        for x, y in baseCoordinates:
-            pygame.draw.circle(screen, WHITE, (x + WIDTH//2, y+HEIGHT//2), 5)
-        
-        player.moveAuto()
-        player.move()
-        player.spawn()
-        player.collision(baseCoordinates)
-        for x, y in targetCoordinate:
-            pygame.draw.circle(screen, YELLOW, (x, y), 5)
-        player.collision2(targetCoordinate)
+    for point in list_point:
+        point.spawn(m)
+    for row in m:
+        print(" ".join('0' if cell == 1 else ' ' for cell in row))
 
-    pygame.display.update() 
+    time.sleep(0.1)
 
-pygame.quit()
